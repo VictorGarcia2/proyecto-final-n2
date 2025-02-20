ccc52d6885d273b304a71c2ed443c781fbd45b4b
VictorGarcia2 <victrgarciaphoto@gmail.com>
2025-02-20 14:33:59 -0600

    Actualizar URL de la API de geocodificación para usar HTTPS en lugar de HTTP

--- src/APIs/Geocoding.js
+++ src/APIs/Geocoding.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const geocoding = (data) => {
     axios({
       method: 'get',
-      url: `http://api.openweathermap.org/geo/1.0/direct?q=${data}&limit=5&appid=02cd14f791e3ce9764a5590ca5580724`,
+      url: `https://api.openweathermap.org/geo/1.0/direct?q=${data}&limit=5&appid=02cd14f791e3ce9764a5590ca5580724`,
     })
     
   }
